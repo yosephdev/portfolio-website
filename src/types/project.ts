c42c5a5e7da436d392744474be17ac6fbfb6ee80
yosephdev <yosephbet@gmail.com>
2025-06-22 19:36:57 +0200

    chore: add TypeScript type definitions

---
+++ src/types/project.ts
@@ -0,0 +1,10 @@
+export interface Project {
+  title: string;
+  description: string;
+  longDescription: string;
+  tech: string[];
+  image: string;
+  screenshots: string[];
+  github?: string;
+  url?: string;
+}
