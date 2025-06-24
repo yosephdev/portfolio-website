3f89df88a3147db17afb89df90efcaa7343f83a3
Yoseph <yosephbet@gmail.com>
2025-06-24 10:21:38 +0200

    Update vite.config.ts to remove hardcoded API key

--- vite.config.ts
+++ vite.config.ts
@@ -12,20 +12,6 @@ export default defineConfig({
     },
     server: {
       port: 5173,
-      host: true,
-      proxy: {
-        '/api/subscribe': {
-          target: 'https://api.beehiiv.com',
-          changeOrigin: true,
-          rewrite: (path) => '/v2/publications/812c37bf-5f3d-46c3-8b13-bc4ab8b18043/subscriptions',
-          configure: (proxy, options) => {
-            proxy.on('proxyReq', (proxyReq, req, res) => {
-              // Add the API key to the request
-              proxyReq.setHeader('Authorization', 'Bearer aR3LJgQ52IZQL7e7sLhP4GCCGCl6g1RgP38g5Z8sWhcWi9rBO9dZdcCFFqy73BSU')
-              proxyReq.setHeader('Content-Type', 'application/json')
-            })
-          }
-        }
-      }
+      host: true
     }
 })
