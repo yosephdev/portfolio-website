6140066d64c964bccbc2bb212234f84185e1c8a3
Yoseph <yosephbet@gmail.com>
2025-07-02 20:45:38 +0200

    feat: Refactor ESLint configuration and install typescript-eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,13 +5,35 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ["dist", "public/downloads/"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        browser: true,
+        es2020: true,
+        node: false,
+        commonjs: false,
+        amd: false,
+        mocha: false,
+        jest: false,
+        jasmine: false,
+        phantomjs: false,
+        protractor: false,
+        qunit: false,
+        jquery: false,
+        prototype: false,
+        yui: false,
+        shelljs: false,
+        worker: false,
+        serviceworker: false,
+        atom: false,
+        ember: false,
+        webextensions: false,
+        greasemonkey: false,
+      },
     },
     plugins: {
       "react-hooks": reactHooks,
@@ -19,10 +41,7 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
+      "react-refresh/only-export-components": "warn",
       "@typescript-eslint/no-unused-vars": "off",
     },
   }
