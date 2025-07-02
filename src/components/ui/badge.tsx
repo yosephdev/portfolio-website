ddf5adc419b05414db9efd80f1a0771691fd08d5
yosephdev <yosephbet@gmail.com>
2025-07-02 21:08:24 +0200

    fix: HelmetProvider, image display, and badge import

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,17 +1,16 @@
 import * as React from "react"
-import * as React from "react"
 
 import { cn } from "@/lib/utils"
 import { badgeVariants, type BadgeVariants } from "@/lib/badgeVariants"
 
 export interface BadgeProps
-  extends React.HTMLAttributes<HTMLDivElement>,
-    BadgeVariants {}
+    extends React.HTMLAttributes<HTMLDivElement>,
+    BadgeVariants { }
 
 function Badge({ className, variant, ...props }: BadgeProps) {
-  return (
-    <div className={cn(badgeVariants({ variant }), className)} {...props} />
-  )
+    return (
+        <div className={cn(badgeVariants({ variant }), className)} {...props} />
+    )
 }
 
 export { Badge }
